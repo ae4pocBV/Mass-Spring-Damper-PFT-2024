377c0fd659517981b6f6fb4681ce3c2fd12f67df
bvormawo <bvormawo@MATHWORKS-QdNJi>
2024-05-17 15:06:54 -0400

    bv20240517 Confirm workflow

--- tests/convecTest.m
+++ tests/convecTest.m
@@ -4,7 +4,7 @@ x = [3-1i, 4+2i, 7-3i];
 y = [8-6i, 12+16i, 40-42i];
 
 assert(isequal(convec(x,y), conv(x,y)), ...
-    "Mex implmentation of convolution does not match MATLAB builtin");
+    "Mex implementation of convolution does not match MATLAB builtin");
 
 
 
